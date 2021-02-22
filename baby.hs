742c925da99c2332f5618f97c9290569393cfc8c
Dolica Akello-Egwel <dolica.akello-egwel@stfc.ac.uk>
2021-02-22 00:33:50 +0000

    Commenting

--- baby.hs
+++ baby.hs
@@ -3,3 +3,7 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
                         else doubleMe x
+
+-- You're allowed to use apostophes in a function name.
+-- A function name needs to start with a lower case character.
+conanO'Brien = "It's a-me, Conan O'Brien!"
