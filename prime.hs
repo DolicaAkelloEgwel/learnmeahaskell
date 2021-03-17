63a48543148176ee29ceaec2684c50c6c11c3398
Dolica Akello-Egwel <dolica.akello-egwel@stfc.ac.uk>
2021-03-17 19:03:51 +0000

    Comments and prime function

--- prime.hs
+++ prime.hs
@@ -1,5 +1,6 @@
 divides d n = rem n d == 0
-ldf k n | divides k n = k
-        | k^2 > n     = n
-        | otherwise   = ldf (k + 1) n
+ldf k n | divides k n = k -- Start by seeing if n can be divided by two
+        | k^2 > n     = n -- Proposition 1.2
+        | otherwise   = ldf (k + 1) n -- Try again with the next number
 ld n = ldf 2 n
+prime n = ld n == n
