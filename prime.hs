7dbc33253b65de9bb427bd8b89a336b298ea9c87
Dolica Akello-Egwel <dolica.akello-egwel@stfc.ac.uk>
2021-03-30 23:24:54 +0100

    Declaring prime type

--- prime.hs
+++ prime.hs
@@ -3,6 +3,8 @@ ldf k n | divides k n = k -- Start by seeing if n can be divided by two
         | k^2 > n     = n -- Proposition 1.2
         | otherwise   = ldf (k + 1) n -- Try again with the next number
 ld n = ldf 2 n
+
+prime :: Integer -> Bool
 prime n | n < 1 = error "not a positive integer"
         | n == 1 = False
         | otherwise = ld n == n
