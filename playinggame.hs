64ab867aa9d47c1eb40f29df8177e1d4c6d13ebc
Dolica Akello-Egwel <dolica.akello-egwel@stfc.ac.uk>
2021-04-07 23:39:45 +0100

    accidentally found this on google

--- playinggame.hs
+++ playinggame.hs
@@ -2,3 +2,7 @@ maxInt :: [Int] -> Int
 maxInt [] = error "empty list"
 maxInt [x] = x
 maxInt (x:xs) = max x (maxInt xs)
+
+removeItem _ []                 = []
+removeItem x (y:ys) | x == y    = removeItem x ys
+                    | otherwise = y : removeItem x ys
