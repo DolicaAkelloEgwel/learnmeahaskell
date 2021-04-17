25d83d2a5c1d3fd35f496779e2a4563c667cd9fb
Dolica Akello-Egwel <dolica.akello-egwel@stfc.ac.uk>
2021-04-17 23:32:17 +0100

    Find min and sort function

--- playinggame.hs
+++ playinggame.hs
@@ -3,6 +3,15 @@ maxInt [] = error "empty list"
 maxInt [x] = x
 maxInt (x:xs) = max x (maxInt xs)
 
+minInt :: [Int] -> Int
+minInt [] = error "empty list"
+minInt [x] = x
+minInt (x:xs) = min x (minInt xs)
+
 removeFirst _ []                 = []
 removeFirst x (y:ys) | x == y    = ys
                      | otherwise = y : removeFirst x ys
+
+srtInts :: [Int] -> [Int]
+srtInts [] = []
+srtInts xs = m : (removeFirst m (srtInts m xs)) where m = mnmInt xs
